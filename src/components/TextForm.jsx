4cc13aad21ed455b7f8513afcb3331dd342ee584
Azeem-13 <82161544+Azeem-13@users.noreply.github.com>
2022-09-14 23:51:50 +0530

    TextForm Buttons Added.

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -1,15 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TextForm = (props) => {
+  const [text, setText] = useState("");
+
+  function handleTextChange(event) {
+    setText(event.target.value);
+  }
+
+  function handleUpperCase() {
+    setText(text.toUpperCase());
+  }
+
+  function handleLowerCase() {
+    setText(text.toLowerCase());
+  }
+
+  function handleCopyText(event) {
+    var text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  }
+
+  function handleTrimText() {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  }
+
+  function handleClearText() {
+    setText("");
+  }
+
   return (
     <div className="container my-2">
+      {/* textarea */}
       <h1>{props.heading}</h1>
       <div class="mb-3">
         <textarea
           class="form-control"
-          id="exampleFormControlTextarea1"
+          id="myBox"
           rows="8"
+          value={text}
+          onChange={handleTextChange}
         ></textarea>
+      </div>
+
+      {/* buttons */}
+      <div className="container my-3">
+        <button className="btn btn-primary mx-2" onClick={handleUpperCase}>
+          UpperCase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleLowerCase}>
+          LowerCase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleCopyText}>
+          Copy
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleTrimText}>
+          Trim
+        </button>
+        <button className="btn btn-primary my-2 mx-2" onClick={handleClearText}>
+          Clear
+        </button>
+      </div>
+
+      {/* Text Summary */}
+      <div className="container my-5">
+        <h1>Text Summary</h1>
+        <p>
+          <li>
+            There are <strong>{text.split(" ").length} words</strong> and{" "}
+            <strong>{text.length} characters</strong> in your test.
+          </li>
+        </p>
+        <p>
+          <li>
+            Its takes <strong>{0.008 * text.split("").length} minutes</strong>{" "}
+            to read your text.
+          </li>
+        </p>
       </div>
     </div>
   );
