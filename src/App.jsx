b91f1af7d3b9f9ba959cbb8ddb9ee680b175a5d7
Azeem-13 <82161544+Azeem-13@users.noreply.github.com>
2022-09-14 23:42:04 +0530

    TextForm textarea added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
 
 const App = () => {
   return (
     <>
       <Navbar title='TextUtils'/>
+      <TextForm heading='Test Your Text'/>
     </>
   );
 };
--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
-const TextForm = () => {
+const TextForm = (props) => {
   return (
-    <div>TextForm</div>
-  )
-}
+    <div className="container my-2">
+      <h1>{props.heading}</h1>
+      <div class="mb-3">
+        <textarea
+          class="form-control"
+          id="exampleFormControlTextarea1"
+          rows="8"
+        ></textarea>
+      </div>
+    </div>
+  );
+};
 
-export default TextForm
+export default TextForm;
